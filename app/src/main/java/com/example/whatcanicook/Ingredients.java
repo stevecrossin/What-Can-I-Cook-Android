a57e6d4ed7e3aa66717fa1508090b6a497e611ed
Steve Crossin <stevecrossin@gmail.com>
2019-03-16 14:28:06 +1100

    Test commit only to ensure signing of code working correctly.
    Signed-off-by: Steve Crossin <stevecrossin@gmail.com>

--- app/src/main/java/com/example/whatcanicook/Ingredients.java
+++ app/src/main/java/com/example/whatcanicook/Ingredients.java
@@ -1,7 +1,7 @@
 package com.example.whatcanicook;
 
 /**
- * This class handles the ingredients functions of the application. Test addition here only for purpose of checking commit signing. Still not working..
+ * This class handles the ingredients functions of the application. Test addition here only for purpose of checking commit signing. Test commit to working
  */
 
 public class Ingredients {
