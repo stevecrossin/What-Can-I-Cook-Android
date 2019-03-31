85bc463767eb8a80ba95deacf0c82d2be3506aef
Steve Crossin <stevecrossin@gmail.com>
2019-03-31 17:37:25 +1100

    Build login database and getter/setter methods
    Signed-off-by: Steve Crossin <stevecrossin@gmail.com>

---
+++ app/src/main/java/com/stevecrossin/whatcanicook/entities/UserDao.java
@@ -0,0 +1,21 @@
+package com.stevecrossin.whatcanicook.entities;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+@Dao
+public interface UserDao {
+    /**
+     * Defines all the operations that are carried out in the database. References UserRecord.java
+     *
+     * @Insert - inserts user record into the database. Conflict strategy is set to fail the insert if the userRecord already exists.
+     */
+
+    @Insert(onConflict = OnConflictStrategy.FAIL)
+    void insertUser(User user);
+
+    @Query("SELECT * from user WHERE user_name=:userName")
+    User getUser(String userName);
+    }
