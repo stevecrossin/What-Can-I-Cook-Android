9b4e9a03c10a3f438be8d118584177c0a179d742
Steve Crossin <stevecrossin@gmail.com>
2019-05-14 02:26:59 +1000

    comment out code not needed, layouts need fixing but can take it from here

--- app/src/main/java/com/stevecrossin/whatcanicook/adapter/MyRecipesViewHolder.java
+++ app/src/main/java/com/stevecrossin/whatcanicook/adapter/MyRecipesViewHolder.java
@@ -2,8 +2,6 @@ package com.stevecrossin.whatcanicook.adapter;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.graphics.Color;
-import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.AppCompatTextView;
@@ -14,15 +12,13 @@ import com.stevecrossin.whatcanicook.R;
 import com.stevecrossin.whatcanicook.entities.Recipe;
 import com.stevecrossin.whatcanicook.roomdatabase.AppDataRepo;
 
-import java.util.ArrayList;
-
 public class MyRecipesViewHolder extends RecyclerView.ViewHolder{
     private AppCompatTextView recipeName;
     private AppCompatImageView recipeImage;
-    private AppCompatTextView recipeMissingIngredients;
+    //private AppCompatTextView recipeMissingIngredients;
     public AppCompatImageView recipeRemoveButton;
     private AppDataRepo repository;
-    ArrayList<String> missingIngredients;
+    //ArrayList<String> missingIngredients;
 
     private static final String TAG = "RecipeViewHolder";
 
@@ -31,7 +27,7 @@ public class MyRecipesViewHolder extends RecyclerView.ViewHolder{
 
         recipeName = itemView.findViewById(R.id.recipe_name);
         recipeImage = itemView.findViewById(R.id.recipe_img);
-        recipeMissingIngredients = itemView.findViewById(R.id.recipe_missing_ingredients);
+        //recipeMissingIngredients = itemView.findViewById(R.id.recipe_missing_ingredients);
         recipeRemoveButton = itemView.findViewById(R.id.remove);
 
         repository = new AppDataRepo(itemView.getContext());
@@ -43,31 +39,33 @@ public class MyRecipesViewHolder extends RecyclerView.ViewHolder{
         int drawableResourceId = context.getResources().getIdentifier(recipe.getRecipeImage(), "drawable", context.getPackageName());
         recipeImage.setImageResource(drawableResourceId);
 
-        new AsyncTask<Void, Void, Void>() {
-            @Override
-            protected Void doInBackground(Void... voids) {
-                missingIngredients = new ArrayList<>(repository.getMissingIngredientsByName(recipe.getRecipeName(), 0));
-                return null;
-            }
+        /**
 
-            @Override
-            protected void onPostExecute(Void aVoid) {
-                super.onPostExecute(aVoid);
-                StringBuilder missing = new StringBuilder();
-                for (String string : missingIngredients)
-                    missing.append(string).append(", ");
-                if (!missing.toString().equals(""))
-                    missing = new StringBuilder(missing.substring(0, missing.length() - 2));
+         new AsyncTask<Void, Void, Void>() {
+        @Override protected Void doInBackground(Void... voids) {
+        missingIngredients = new ArrayList<>(repository.getMissingIngredientsByName(recipe.getRecipeName(), 0));
+        return null;
+        }
 
-                if (missingIngredients.size() > 0) {
-                    recipeMissingIngredients.setTextColor(Color.RED);
-                    recipeMissingIngredients.setText(String.format("You need: %s for this recipe", missing.toString()));
-                } else {
-                    recipeMissingIngredients.setTextColor(Color.parseColor("#006400"));
-                    recipeMissingIngredients.setText(context.getString(R.string.enoughIngredients));
-                }
+        @Override protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+        StringBuilder missing = new StringBuilder();
+        for (String string : missingIngredients)
+        missing.append(string).append(", ");
+        if (!missing.toString().equals(""))
+        missing = new StringBuilder(missing.substring(0, missing.length() - 2));
 
-            }
+        if (missingIngredients.size() > 0) {
+        recipeMissingIngredients.setTextColor(Color.RED);
+        recipeMissingIngredients.setText(String.format("You need: %s for this recipe", missing.toString()));
+        } else {
+        recipeMissingIngredients.setTextColor(Color.parseColor("#006400"));
+        recipeMissingIngredients.setText(context.getString(R.string.enoughIngredients));
+        }
+
+        }
         }.execute();
+         }
+         */
     }
 }
