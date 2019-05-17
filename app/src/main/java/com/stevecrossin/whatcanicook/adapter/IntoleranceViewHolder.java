02d73b9ce124c0f2278ecdc21f03cd35bce5651e
Steve Crossin <stevecrossin@gmail.com>
2019-05-18 02:34:29 +1000

    commit comments of code progressively (to help check off files as they're done)

--- app/src/main/java/com/stevecrossin/whatcanicook/adapter/IntoleranceViewHolder.java
+++ app/src/main/java/com/stevecrossin/whatcanicook/adapter/IntoleranceViewHolder.java
@@ -6,7 +6,6 @@ import android.support.annotation.NonNull;
 import android.support.v7.widget.AppCompatCheckBox;
 import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
 
@@ -17,11 +16,14 @@ import com.stevecrossin.whatcanicook.roomdatabase.AppDataRepo;
 import java.util.ArrayList;
 
 class IntoleranceViewHolder extends RecyclerView.ViewHolder {
-    private static final String TAG = "IngredientViewHolder";
     private AppCompatTextView intoleranceName;
     private AppCompatCheckBox intoleranceCheckBox;
     private AppDataRepo repository;
 
+    /**
+     * Initialise the fields of each row in the viewholder - in this case the name of the intolerance and the checkbox.
+     * Also creates a new instance of the AppDataRepo. This view is also not recyclable to prevent scrolling issues
+     */
     IntoleranceViewHolder(@NonNull View itemView) {
         super(itemView);
         this.setIsRecyclable(false);
@@ -30,6 +32,12 @@ class IntoleranceViewHolder extends RecyclerView.ViewHolder {
         repository = new AppDataRepo(itemView.getContext());
     }
 
+    /**
+     * Binds each row in the view holder to a record in the intolerances database and sets text in row to that database record.
+     * Checks the intolerance database to see if the intolerance is noted as "selected", and if so, sets the checkbox to "checked"
+     * otherwise the intolerance is not checked. Also contains instance of checkchangedlistener - once the intolerance is selected/deselected
+     * this text is taken and converted to a string
+     **/
     void bindRow(Intolerance intolerance) {
         intoleranceName.setText(intolerance.getIntoleranceName());
         if (intolerance.isIntoleranceSelected())
@@ -40,21 +48,18 @@ class IntoleranceViewHolder extends RecyclerView.ViewHolder {
         intoleranceCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                Log.d(TAG, "onCheckedChanged: " + intoleranceName.getText());
                 intoleranceSelected(isChecked, intoleranceName.getText().toString());
             }
         });
     }
 
+    /**
+     * This method is caled when an intolerance has been updated in the activity. This will perform an AsyncTask to call the database to change the value
+     * of that intolerance to either selected (intoleranceSelected=1), or deselected (intoleranceSelected=0), based on the value of the string passed from bindrow() and whether the checkbox was ticked or unselected.
+     * After this task is completed, the view will also be refreshed with this updated value (checked or not checked)
+     */
     @SuppressLint("StaticFieldLeak")
     private void intoleranceSelected(final boolean isSelected, final String intoleranceName) {
-        /*
-        This method is responsible for taking action when an intolerance is updated in the Intolerance activity
-        When a user clicks on the on/off slider to mark it as active/inactive, in a background task, this information will be passed to the Users.updateUser method
-        which will take appropriate action to update the user database.
-        The intolerances database will also be updated to mark the relevant intolerance as active/inactive.
-        The Intolerance activity UI will also be refreshed so that the intolerance clicked is made active/inactive.
-        */
 
         new AsyncTask<Void, Void, Void>() {
             @Override
@@ -66,7 +71,6 @@ class IntoleranceViewHolder extends RecyclerView.ViewHolder {
                     for (Intolerance intolerance : list) {
                         repository.excludeIngredient(intolerance.getIngredientName());
                         repository.excludeRecipe(intolerance.getIngredientName());
-                        Log.d(TAG, "Exclude ingredient: " + intolerance.getIngredientName());
                     }
                 } else {
                     repository.includeIntolerance(intoleranceName);
@@ -74,7 +78,6 @@ class IntoleranceViewHolder extends RecyclerView.ViewHolder {
                     for (Intolerance intolerance : list) {
                         repository.includeIngredient(intolerance.getIngredientName());
                         repository.includeRecipe(intolerance.getIngredientName());
-                        Log.d(TAG, "Include ingredient: " + intolerance.getIngredientName());
                     }
                 }
                 return null;
