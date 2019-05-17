200a8da98fb7f28a71f6dfeeddb05484395d396b
Steve Crossin <stevecrossin@gmail.com>
2019-05-18 02:33:42 +1000

    commit comments of code progressively (to help check off files as they're done)

--- app/src/main/java/com/stevecrossin/whatcanicook/adapter/IntoleranceViewAdapter.java
+++ app/src/main/java/com/stevecrossin/whatcanicook/adapter/IntoleranceViewAdapter.java
@@ -19,21 +19,28 @@ public class IntoleranceViewAdapter extends RecyclerView.Adapter<IntoleranceView
 
 
     /**
-     * Creates a new instance of Intolerance Adapter, which contains the dietary requirements to be displayed in the adapter and the rowclicklistener
+     * Creates a new instance of Intolerance Adapter, which contains the elements, being the dietary requirements to be displayed in the adapter and the rowclicklistener
      */
     public IntoleranceViewAdapter(ArrayList<Intolerance> intoleranceList, IntoleranceViewAdapter.rowClickedListener rowClickedListener) {
         this.intoleranceList = intoleranceList;
         this.rowClickedListener = rowClickedListener;
     }
 
+    /**
+     * Initialise IntoleranceViewHolder with the Row View that is to be inflated. Uses intolerance_row as the layout for each row in the viewholder
+     */
     @NonNull
     @Override
     public IntoleranceViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         return new IntoleranceViewHolder(LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.intolerance_row, viewGroup, false));
     }
 
+    /**
+     * Override method for the RecyclerView, which is called to display the data at the specified position.
+     * First, the intolerances are fetched and bound to the viewholder with the bindRow method. Then, an event listener is setup for each row when clicked.
+     */
     @Override
-    public void onBindViewHolder(final IntoleranceViewHolder intoleranceViewHolder, int i) {
+    public void onBindViewHolder(@NonNull final IntoleranceViewHolder intoleranceViewHolder, int i) {
         intoleranceViewHolder.bindRow(intoleranceList.get(i));
         intoleranceViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,16 +50,26 @@ public class IntoleranceViewAdapter extends RecyclerView.Adapter<IntoleranceView
         });
     }
 
+    /**
+     * Sets the size of the adapter which contains the number of items that will be shown in the RecyclerView.
+     * This is based on the size (number) of intolerances that exist
+     */
     @Override
     public int getItemCount() {
         return intoleranceList.size();
     }
 
+    /**
+     * Updates the list with new values, and then notifies DB that the data has been changed
+     */
     public void updateIntolerances(List<Intolerance> intolerances) {
         this.intoleranceList = intolerances;
         notifyDataSetChanged();
     }
 
+    /**
+     * Interface that has the onRowClicked method to implement
+     */
     public interface rowClickedListener {
         void onRowClicked(Intolerance into);
     }
