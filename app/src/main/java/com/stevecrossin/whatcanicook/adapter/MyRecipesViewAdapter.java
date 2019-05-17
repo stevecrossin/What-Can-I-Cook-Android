21f762484fa03f9b2bcf494ef94af9cbd33c9320
Steve Crossin <stevecrossin@gmail.com>
2019-05-18 02:36:50 +1000

    commit comments of code progressively (to help check off files as they're done)

--- app/src/main/java/com/stevecrossin/whatcanicook/adapter/MyRecipesViewAdapter.java
+++ app/src/main/java/com/stevecrossin/whatcanicook/adapter/MyRecipesViewAdapter.java
@@ -23,13 +23,19 @@ public class MyRecipesViewAdapter extends RecyclerView.Adapter<MyRecipesViewHold
     private AppDataRepo repo = new AppDataRepo(context);
 
 
-    //Adapter for Recyclerview of Saved Recipes. Contains onClick listeners for the clicking of the recipes and also of the X that removes recipes
+    /**
+     * Initialise the fields of each row in the viewholder - in this case the name of the recipe, and onClick listeners to detemine if the row was clicked
+     * (which causes the UI to navigate to the recipe itself,  and the close element, which causes the recipe to be removed  - these are all per the defined methods in the SavedRecipes class.
+     */
     public MyRecipesViewAdapter(ArrayList<Recipe> recipes, MyRecipesViewAdapter.rowClickedListener rowClickedListener, MyRecipesViewAdapter.removeClickedListener removeClickedListener) {
         this.recipes = recipes;
         this.rowClickedListener = rowClickedListener;
         this.removeClickedListener = removeClickedListener;
     }
 
+    /**
+     * Initialise MyRecipesViewHolder with the Row View that is to be inflated. Uses saved_recipe_row as the layout for each row in the viewholder
+     */
     @NonNull
     @Override
     public MyRecipesViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
@@ -37,6 +43,12 @@ public class MyRecipesViewAdapter extends RecyclerView.Adapter<MyRecipesViewHold
         return new MyRecipesViewHolder(LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.saved_recipe_row, viewGroup, false));
     }
 
+    /**
+     * Override method for the RecyclerView, which is called to display the data at the specified position.
+     * First, the select recipes are fetched and bound to the viewholder with the bindRow method. Then, an event listener is setup for each row is clicked.
+     * <p>
+     * Additionally, an onClick listener exists for the removeRecipe UI element. This UI element controls the removal of recipes. When this element is clicked, it gets the position of the row in the adapter and gets the recipe name.
+     */
     @Override
     public void onBindViewHolder(@NonNull final MyRecipesViewHolder recipeViewHolder, @SuppressLint("RecyclerView") final int i) {
         final Recipe recipe = recipes.get(i);
@@ -57,10 +69,11 @@ public class MyRecipesViewAdapter extends RecyclerView.Adapter<MyRecipesViewHold
     }
 
     /**
-     * Task to remove the recipe. This is performed in background. If the recipe was a saved recipe, it will be "unsaved" otherwise if it was a custom
-     * recipe it will be removed from the DB
+     * Task to remove the recipe. This is performed in the background via AsyncTask to either the unSaveRecipe method, or the removeRecipe method in AppDB, based on whether the recipe was already in the DB
+     * but "saved", or a custom recipe that was added by the user. These operations will then perform the relevant database function, and then once completed, the recipe will be removed from the ViewHolder and the list will be updated without the removed item
      */
 
+    @SuppressLint("StaticFieldLeak")
     public void removeRecipefromSaved(final Recipe recipe, final MyRecipesViewHolder recipeViewHolder) {
         new AsyncTask<Void, Void, Void>() {
             @Override
@@ -73,7 +86,6 @@ public class MyRecipesViewAdapter extends RecyclerView.Adapter<MyRecipesViewHold
                 return null;
             }
 
-            //After the task has been executed, the recipe row will be removed from the viewholder
             @Override
             protected void onPostExecute(Void aVoid) {
                 super.onPostExecute(aVoid);
@@ -83,24 +95,33 @@ public class MyRecipesViewAdapter extends RecyclerView.Adapter<MyRecipesViewHold
         }.execute();
     }
 
-    //Gets count of items to be in recyclerview? - trong - help
+    /**
+     * Sets the size of the adapter which contains the number of items that will be shown in the RecyclerView.
+     * This is based on the size (number) of saved recipes that exist in the database
+     */
     @Override
     public int getItemCount() {
         return recipes.size();
     }
 
-    //Updates recipes in the view - again help!
+    /**
+     * Updates the list with new values, and then notifies DB that the data has been changed
+     */
     public void updateRecipes(ArrayList<Recipe> recipes) {
         this.recipes = recipes;
         notifyDataSetChanged();
     }
 
-    //Listener for row click
+    /**
+     * Interface that has the onRowClicked method to implement
+     */
     public interface rowClickedListener {
         void onRowClicked(Recipe recipe);
     }
 
-    //Listener for remove click
+    /**
+     * Interface that has the onRemoveClicked method to implement
+     */
     public interface removeClickedListener {
         void onRemoveClicked(Recipe recipe, MyRecipesViewHolder recipeViewHolder);
     }
