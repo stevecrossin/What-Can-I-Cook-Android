037cfa91301b447413039f8f477586d6307736a9
Steve Crossin <stevecrossin@gmail.com>
2019-05-18 02:06:51 +1000

    commit comments of code progressively (to help check off files as they're done)

--- app/src/main/java/com/stevecrossin/whatcanicook/adapter/MyRecipesViewHolder.java
+++ app/src/main/java/com/stevecrossin/whatcanicook/adapter/MyRecipesViewHolder.java
@@ -16,6 +16,9 @@ public class MyRecipesViewHolder extends RecyclerView.ViewHolder {
     private AppCompatImageView recipeImage;
     AppCompatImageView recipeRemoveButton;
 
+    /**
+     * Initialise the fields of each row in the viewholder - in this case the name of the recipe, the image, and the button to remove the recipe.
+     */
     MyRecipesViewHolder(@NonNull View itemView) {
         super(itemView);
 
@@ -24,6 +27,10 @@ public class MyRecipesViewHolder extends RecyclerView.ViewHolder {
         recipeRemoveButton = itemView.findViewById(R.id.remove);
     }
 
+    /**
+     * Binds each row in the view holder to a record in the recipes database and sets text in row to that respective database record
+     * Also sets the recipe image in that row based on the drawable resource for that recipe in the database, if it exists.
+     */
     @SuppressLint("StaticFieldLeak")
     void bindRow(final Recipe recipe, final Context context) {
         recipeName.setText(recipe.getRecipeName());
