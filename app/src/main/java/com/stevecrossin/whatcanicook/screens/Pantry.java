e9997bd584319f75f46f9ce0c9886d81d7577a59
Steve Crossin <stevecrossin@gmail.com>
2019-05-18 20:02:03 +1000

    commit pantry comments

--- app/src/main/java/com/stevecrossin/whatcanicook/screens/Pantry.java
+++ app/src/main/java/com/stevecrossin/whatcanicook/screens/Pantry.java
@@ -26,16 +26,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-/*
-This class covers the Pantry screen and database. CategoryPicker added here will be stored in a Room Database
-called Pantry, with ingredients selected by the user displayed
- */
 public class Pantry extends AppCompatActivity {
 
     private AutoCompleteTextView autoCompleteTextView;
     private RecyclerView recyclerView;
 
-    /*Set view as pantry activity*/
+    /**
+     * On creation of the activity, perform these functions.
+     * Set the current view as the activity_pantry XML and load the UI elements in that XML file into that view.
+     * Load Google Ads for the activity and send an adRequest to load an ad.
+     * Call initView, getPantryIngredient and loadPantry methods.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,11 +52,15 @@ public class Pantry extends AppCompatActivity {
 
     /**
      * This is method is responsible for setting the OnClick listener for the Pantry autocompleteTextView.
-     * OnThe OnClick listener has multiple elements, to handle clearing users ingredients from the pantry, delete individual ingredients
-     * from the pantry and for handling actions when the end user performs actions.
+     * The OnClick listener has multiple elements, to handle clearing users ingredients from the pantry and deleting individual user ingredients,
+     * depending on whether the end user clicks the "Clear Ingredients" button or an individual ingredient row.
+     *
      * <p>
      * It also initialises the view by finding the elements that should be displayed in the view, specifically the autocomplete textview and the recycler view, also sets the adapter for the
      * recycler view
+     * <p>
+     * When an onClick method is triggered, an async task is called in the background which creates a new instance of AppDataRepo
+     * and calls the methods to clear the ingredient from the pantry database. Once this is completed the RecyclerView will be updated.
      */
     private void initView() {
         autoCompleteTextView = findViewById(R.id.pantryIngredientsSearchBox);
@@ -98,8 +103,11 @@ public class Pantry extends AppCompatActivity {
     }
 
     /**
-     * This is method is responsible for getting a list of all ingredients from the appdata repo and returning them into a new instance of the Pantry AutoComplete Adapter
-     * */
+     * This is method is responsible for getting a list of all ingredients that are to be selectable in the Pantry
+     * autocomplete textbox. It calls an Async task which will run the getAllTolerant ingredients from the appdata repo -
+     * ingredients that are in the database that the user hasn't noted an intolerance to, and return these to a new instance of the
+     * Pantry AutoComplete adapter.
+     */
     @SuppressLint("StaticFieldLeak")
     public void getPantryIngredient() {
         final AppDataRepo repository = new AppDataRepo(this);
@@ -123,11 +131,14 @@ public class Pantry extends AppCompatActivity {
 
 
     /**
-     * This adds the ingredient in the pantry to the pantry database. It is possible for the end user to attempt to add an ingredient to the pantry twice - this causes an
+     * This method handles the adding of ingredients that were added into the pantry, into the pantry database.
+     * This is performed as an Async task, which will call the AppRepo method to add the ingredient to the pantry.
+     * <p>
+     * It is possible for the end user to attempt to add an ingredient to the pantry twice - this causes an
      * SQLLiteConstraint exception which has been caught. A toast will also be shown to the user advising them that the ingredient already exists in the pantry.
-     * caught to
+     * <p>
+     * After this background task is completed, the recyclerView will be updated with the new list of ingredients that exist in the pantry.
      */
-
     @SuppressLint("StaticFieldLeak")
     public void addPantryIngredientToDB(final Ingredient ingredient) {
         new AsyncTask<Void, Void, String>() {
@@ -159,17 +170,13 @@ public class Pantry extends AppCompatActivity {
     }
 
     /**
-     * This will load the contents of the pantry database
+     * This method will load the contents of the pantry database. It is called when the user clicks the "My Pantry" button.
+     * It will:
+     * 1. Perform a query of the pantry database via an async task referenced in the AppDataRepo to get all records in the database, and return these values
+     * 2. Pass that data to the pantry scene, and update the recyclerview with the items in the database
      */
-
     @SuppressLint("StaticFieldLeak")
     public void loadPantry() {
-        /*
-        This method will be called when a user clicks the "My Pantry" button, or is otherwise navigated back to the pantry scene through the app
-        1. Perform a query of the pantry database, and get all records in the database
-        2. Pass that data to the pantry scene, and update the recyclerview with the items in the database
-        */
-
         new AsyncTask<Void, Void, List<Ingredient>>() {
 
             @Override
@@ -184,8 +191,5 @@ public class Pantry extends AppCompatActivity {
                 ((PantryRecycleViewAdapter) Objects.requireNonNull(recyclerView.getAdapter())).updateCategories(ingredients);
             }
         }.execute();
-
-
     }
-
 }
