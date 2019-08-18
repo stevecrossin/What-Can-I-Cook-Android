6b1f227886d3ee3625a51cf97935d0631aee8198
Steven Crossin <stevecrossin@gmail.com>
2019-08-18 22:12:23 +1000

    update version increment

--- app/src/main/java/com/stevecrossin/whatcanicook/utils/Update.java
+++ app/src/main/java/com/stevecrossin/whatcanicook/utils/Update.java
@@ -5,9 +5,9 @@ public class Update {
     /*
     * We will have this value in both DB & Code.
     * Compare this with DB to decide whether to reset the db or not.
-    * When I publish an app update that changes CSV files and needs DB update, increment this by one.
+    * When I publish an app update that changes CSV files and needs DB update, increment this by one. Currently it is at 2
     *
     *  */
-    public static int LAST_UPDATE_VERSION = 1;
+    public static int LAST_UPDATE_VERSION = 2;
 
 }
